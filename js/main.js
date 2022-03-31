6a867c26346a127bb687d72fccc310ccf54c55c0
Sergei <pylypenko.sn@gmail.com>
2022-03-31 23:16:22 +0300

    add modal window

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openFormApp() {
+    document.querySelector('.form-application-container').classList.add("form-application-container--visible");
+}
+
+function closeFormApp() {
+    document.querySelector('.form-application-container').classList.remove('form-application-container--visible');
+}
