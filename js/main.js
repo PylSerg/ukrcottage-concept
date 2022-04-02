dc96ac60718a14354ce24ddd241efc29c685a235
Sergei <pylypenko.sn@gmail.com>
2022-04-02 20:48:35 +0300

    fix modal window

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 function openFormApp() {
     document.querySelector('.form-application-container').classList.add("form-application-container--visible");
+    document.querySelector('body').style.overflow = "hidden";
 }
 
 function closeFormApp() {
     document.querySelector('.form-application-container').classList.remove('form-application-container--visible');
+        document.querySelector('body').style.overflow = "scroll";
+
 }
