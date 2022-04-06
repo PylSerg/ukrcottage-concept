2f395e4f2f301e6c35eccb1ab0142cb13e352447
Sergei <pylypenko.sn@gmail.com>
2022-04-06 13:29:22 +0300

    add menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ function closeFormApp() {
         document.querySelector('body').style.overflow = "scroll";
 
 }
+
+(() => {
+    const menuBtn = document.querySelector("[data-menu-button]");
+
+    menuBtn.addEventListener("click", () => {
+        menuBtn.classList.toggle("active-button");
+    });
+})();
+
+function toTop() {
+    let t;
+    let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+    if(top > 0) {
+        window.scrollBy(0,-100);
+        t = setTimeout('toTop()',20);
+    } else clearTimeout(t);
+    return false;
+}
