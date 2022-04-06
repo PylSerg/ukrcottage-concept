368536e28063856ce10436c6b6b1743c0b6eee03
Sergei <pylypenko.sn@gmail.com>
2022-04-06 22:11:50 +0300

    add search bar

--- js/main.js
+++ js/main.js
@@ -23,17 +23,22 @@ function toTop() {
 const searchBtn = document.querySelector("[data-search-button]");
 const menuBtn = document.querySelector("[data-menu-button]");
 const contactsBtn = document.querySelector("[data-contacts-button]");
+const toTopBtn = document.querySelector("[data-up-button]");
 
 const menuList = document.querySelector("[data-menu]");
 const contactsList = document.querySelector("[data-contacts]");
+const searchList = document.querySelector("[data-search]");
 
 (() => {
 
     searchBtn.addEventListener("click", () => {
         searchBtn.classList.toggle("active-button");
+        searchList.classList.toggle("visible-search");
 
         menuBtn.classList.remove("active-button");
-        menuList.classList.remove("visible-menu");       
+        menuList.classList.remove("visible-menu");
+        contactsBtn.classList.remove("active-button");
+        contactsList.classList.remove("visible-contacts");
     });   
 
     menuBtn.addEventListener("click", () => {
@@ -41,13 +46,38 @@ const contactsList = document.querySelector("[data-contacts]");
         menuList.classList.toggle("visible-menu");
 
         searchBtn.classList.remove("active-button");
+        searchList.classList.remove("visible-search");
+        contactsBtn.classList.remove("active-button");
+        contactsList.classList.remove("visible-contacts");
     });
 
     contactsBtn.addEventListener("click", () => {
         contactsBtn.classList.toggle("active-button");
         contactsList.classList.toggle("visible-contacts");
 
+        searchBtn.classList.remove("active-button");
+        searchList.classList.remove("visible-search");
         menuBtn.classList.remove("active-button");
         menuList.classList.remove("visible-menu");       
-    });    
+    });
+    
+        toTopBtn.addEventListener("click", () => {
+
+        searchBtn.classList.remove("active-button");
+        searchList.classList.remove("visible-search");
+        menuBtn.classList.remove("active-button");
+        menuList.classList.remove("visible-menu");
+        contactsBtn.classList.remove("active-button");
+        contactsList.classList.remove("visible-contacts");
+    }); 
+})();
+
+(() => {
+    const searchStart = document.querySelector("[data-search-start]");
+
+    searchStart.addEventListener("click", () => {
+        searchBtn.classList.remove("active-button");
+        searchList.classList.remove("visible-search");
+        document.querySelector(".search__field").value = "";
+    })
 })();
